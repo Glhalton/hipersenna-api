c1a770a306dcb8c37dde6648d3c288c7e7b3ff6a
Wladimir Moraes Oliveira <76710345+wladimiroliveira@users.noreply.github.com>
2025-09-23 19:49:29 +0000

    feat(signUpResponse): add access level and branch id in route response

--- src/modules/users/service.ts
+++ src/modules/users/service.ts
@@ -1,7 +1,8 @@
 import { auth } from "../../lib/auth";
+import { prisma } from "../../lib/prisma";
 
 export async function signUpService(data: any) {
-    return await auth.api.signUpEmail({
+    const responseSignUp = await auth.api.signUpEmail({
         body: {
             email: data.email,
             name: data.name,
@@ -12,6 +13,24 @@ export async function signUpService(data: any) {
             access_level: data.access_level
         },
     });
+
+    const responseUser = await prisma.hsemployees.findUnique({
+        where: {
+            id: responseSignUp.user.id
+        }
+    })
+
+    return {
+        token: responseSignUp.token,
+        id: responseUser?.id,
+        email: responseUser?.email,
+        name: responseUser?.name,
+        username: responseUser?.username,
+        branchId: responseUser?.branch_id,
+        accessLevel: responseUser?.access_level,
+        createdAt: responseUser?.created_at,
+        modifiedAt: responseUser?.modified_at
+    }
 }
 
 export async function signInService(data: any) {
