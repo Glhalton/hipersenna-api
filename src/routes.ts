d6ee1a75b4e1d630ea6f7c8ca2cab631a464b00c
wladimiroliveira <wladimir2020moraes@outlook.com>
2025-09-25 18:01:34 -0300

    feat (bonus): add the bonus get route

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 import validitiesRoutes from "./modules/validities/routes";
 import { validityRequestsRoutes } from "./modules/validityRequests/routes";
 import userAuthRoutes from "./modules/users/routes";
+import bonusRoutes from "./modules/bonus/routes";
 
 export default async function (app:FastifyInstance){
     app.register(validitiesRoutes, {prefix: '/validities'});
     app.register(validityRequestsRoutes, {prefix: 'validityRequests'});
     app.register(userAuthRoutes, {prefix: '/auth'});
+    app.register(bonusRoutes, {prefix: '/bonus'})
 }
