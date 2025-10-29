955306703c5d3acde95700c3c56ef2d0fb1f2e30
Glhalton <Glhalton.amaro@gmail.com>
2025-10-28 21:19:56 -0300

    docker implementation by creating dockerfile and docker compose

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:20
+
+WORKDIR /app
+
+COPY . .
+
+RUN rm -rf node_modules
+RUN npm install
+
+CMD ["npm", "run", "dev"]
+
+EXPOSE 3333
--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,4 @@
 import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-import { prisma } from "./lib/prisma.js";
-import jwt from "jsonwebtoken";
 import signinRoutes from "./routes/signin.routes.js";
 import usersRoutes from "./routes/users.routes.js";
 import productsRoutes from "./routes/products.routes.js";
@@ -12,49 +10,7 @@ import rolesRoutes from "./routes/roles.routes.js";
 import userRolesRoutes from "./routes/userRoles.routes.js";
 import rolesPermissionsRoutes from "./routes/rolesPermissions.routes.js";
 import userPermissionsRoutes from "./routes/userPermissions.routes.js";
-
-declare module "fastify" {
-  export interface FastifyRequest {
-    user?: {
-      id: number;
-      name: string;
-      username: string;
-      winthor_id: number;
-      branch_id: number;
-      iat: number;
-      exp: number;
-    };
-  }
-}
-
-async function authenticate(request: FastifyRequest, reply: FastifyReply) {
-  try {
-    const token = request.headers.authorization?.replace("Bearer ", "");
-
-    if (!token) {
-      return reply.status(401).send({ message: "Token não fornecido!" });
-    }
-
-    const jwtSecret = process.env.JWT_SECRET;
-    if (!jwtSecret) {
-      throw new Error("JWT_SECRET não definido no .env");
-    }
-
-    const decodedJwt = jwt.verify(token, jwtSecret);
-
-    const session = await prisma.hssessions.findUnique({
-      where: { token },
-    });
-
-    if (typeof decodedJwt === "string" || !session) {
-      return reply.status(401).send({ message: "Token inválido ou expirado" });
-    }
-
-    request.user = decodedJwt as FastifyRequest["user"];
-  } catch (err: any) {
-    reply.status(401).send({ message: `Autenticação falhou: ${err.message}` });
-  }
-}
+import { authenticate } from "./middlewares/authentication.js";
 
 export default async function (app: FastifyInstance) {
   app.register(signinRoutes, { prefix: "/signin" });
