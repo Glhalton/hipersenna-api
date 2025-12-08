5722c27a6857470fcf2feb9a0cc18393e5dee788
Glhalton <Glhalton.amaro@gmail.com>
2025-12-08 15:03:14 -0300

    fix: remove prisma generated from commit

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/__tests__/**/*.ts", "**/?(*.)+(spec|test).ts"],
-
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  transform: {},
 };
