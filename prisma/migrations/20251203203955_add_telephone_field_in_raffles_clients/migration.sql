202644a4570782a2aee1998d2fe0bbef500facd3
Glhalton <Glhalton.amaro@gmail.com>
2025-12-08 15:25:35 -0300

    refactor: refactor prisma migrations

--- prisma/migrations/20251203203955_add_telephone_field_in_raffles_clients/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `telephone` to the `hsraffle_clients` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "hsraffle_clients" ADD COLUMN     "telephone" TEXT NOT NULL;
--- prisma/migrations/20251208181200_set_role_optional/migration.sql
+++
@@ -1,9 +0,0 @@
--- DropForeignKey
-ALTER TABLE "hsemployees" DROP CONSTRAINT "hsemployees_role_id_fkey";
-
--- AlterTable
-ALTER TABLE "hsemployees" ALTER COLUMN "role_id" DROP NOT NULL,
-ALTER COLUMN "role_id" DROP DEFAULT;
-
--- AddForeignKey
-ALTER TABLE "hsemployees" ADD CONSTRAINT "hsemployees_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "hsroles"("id") ON DELETE SET NULL ON UPDATE CASCADE;
